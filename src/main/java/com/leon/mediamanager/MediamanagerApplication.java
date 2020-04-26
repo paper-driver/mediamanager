cc17f78e22e64fe44552de28d498338753b4e5b8
Leon Mao <yuxuan.mao.1@gmail.com>
2020-04-26 18:38:43 -0400

    first commit

---
+++ src/main/java/com/leon/mediamanager/MediamanagerApplication.java
@@ -0,0 +1,13 @@
+package com.leon.mediamanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MediamanagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MediamanagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/leon/mediamanager/MediamanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leon.mediamanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MediamanagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
