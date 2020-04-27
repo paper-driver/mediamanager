49acac81bf72c5ca1700b6ee2956b6e2d0f47233
Leon Mao <yuxuan.mao.1@gmail.com>
2020-04-27 15:31:28 -0400

    incomplete restful service

--- src/main/java/com/leon/mediamanager/MediamanagerApplication.java
+++ src/main/java/com/leon/mediamanager/MediamanagerApplication.java
@@ -1,10 +1,19 @@
 package com.leon.mediamanager;
 
+import com.leon.service.RESTfulService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
 
 @SpringBootApplication
 public class MediamanagerApplication {
+
+	private static final Logger logger = LoggerFactory.getLogger(MediamanagerApplication.class);
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(MediamanagerApplication.class, args);
---
+++ src/main/java/com/leon/service/RESTfulService.java
@@ -0,0 +1,11 @@
+package com.leon.service;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class RESTfulService {
+
+    @PostMapping("users/authenticate")
+    public
+}
